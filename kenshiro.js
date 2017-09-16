126f66618c29e46ac50d4c94985659bc113abab1
Peter Xu <peterxu30@users.noreply.github.com>
2017-09-16 19:55:13 -0400

    got adding images and movement to work

--- kenshiro.js
+++ kenshiro.js
@@ -1,28 +1,62 @@
-kenshiro.KenshiroImage = {
-    WALKING_KENSHIRO = "walking_kenshiro.jpg",
-    RUNNING_KENSHIRO = "running_kenshiro.jpg",
-    PUNCHING_KENSHIRO = "punching_kenshiro.jpg",
-    HEAD_KENSHIRO = "head_kenshiro.jpg"
-}
+$(document).ready(function () {
+
+// KenshiroImage = {
+//     WALKING_KENSHIRO = "walking_kenshiro.jpg",
+//     RUNNING_KENSHIRO = "running_kenshiro.jpg",
+//     PUNCHING_KENSHIRO = "punching_kenshiro.jpg",
+//     HEAD_KENSHIRO = "head_kenshiro.jpg"
+// }
 // rotation is a boolean flag.
-func newKenshiro(idName, className, imageName, x, y, width, height, moveFunction, rotation) {
+function newKenshiro(idName, className, imageName, x, y, width, height, xMovement, yMovement, moveFunction, rotation) {
 
     //create the element in the html
     addImage(imageName, idName, className, x, y, width, height, "kenshiro_end");
 
     var kenshiro = {
         id: idName,
-        class: className,
-        width: width,
-        height: height,
-        xMovement: xMovement,
-        yMovement: yMovement,
-    };
-
-    setInterval(moveFunction, 30); //$("#image").css({left:value.val, top:value.val});
-
-    return {
-        id: idName,
         class: className
     };
+    console.log(kenshiro.id);
+    actualMoveFunction = moveFunction(kenshiro, xMovement, yMovement);
+    setInterval(actualMoveFunction, 30);
+    return kenshiro;
 }
+
+function slidingKenshiroMoveFunctionGenerator(kenshiro, xMovement, yMovement) {
+    var kenshiroElem = document.getElementById(kenshiro.id);
+    var rect = kenshiroElem.getBoundingClientRect();
+    var count = {x: rect.left, y: rect.top};
+    return function() {
+        console.log(rect.top, rect.left, xMovement);
+
+        var elem = document.getElementById(kenshiro.id);
+        var newX = elem.style.top + xMovement;
+        elem.style.left = count.x + xMovement;
+        elem.style.top = count.y + yMovement;
+        
+        var rect1 = elem.getBoundingClientRect();
+        count.x = rect1.left;
+        count.y = rect1.top;
+
+        document.getElementById(kenshiro.id).style.left += yMovement;
+    };
+}
+
+function newBouncingKenshiro(idName, className, imageName, x, y, width, height, moveFunction, rotation) {
+    newKenshiro(idName, className, imageName, x, y, width, height, moveFunction, rotation);
+}
+
+newKenshiro("test1", "test", "img/kenshiro.png", 0, 0, 100, 100, 10, 0, slidingKenshiroMoveFunctionGenerator, false);
+newKenshiro("test2", "test", "img/kenshiro.png", 1000, 100, 100, 100, -10, 0, slidingKenshiroMoveFunctionGenerator, false);
+
+
+// func bouncingKenshiroMoveFunctionGenerator(kenshiro, xMovement, yMovement) {
+//     var kenshiroElem = $(kenshiro.id);
+//     var rect = kenshiroElem.getBoundingClientRect();
+//     var coords = {top: rect.top, left: rect.left};
+//     return func() {
+//         kenshiroElem.css({left: coords.top + xMovement, top: coords.left + yMovement});
+
+//     };
+// }
+});
--- utils.js
+++ utils.js
@@ -8,7 +8,10 @@ function addImage (imageUrl, idName, className, x, y, width, height, beforeEleme
   newDiv.id = idName;
   newDiv.class = className; //image from css
   newDiv.appendChild(newImg); //add the text node to the newly created div. 
-  newDiv.css({left:x, top:y});
+  newDiv.style.left = x;
+  newDiv.style.top = y;
+  newDiv.style.background = "red";
+  newDiv.style.position = "absolute";
 
   // add the newly created element and its content into the DOM 
   var currentDiv = document.getElementById(beforeElementId); //this div is the reference point in the HTML for new images.
